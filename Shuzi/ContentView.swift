1d249ba4ba5a72892a392a701cafb46d587fd8a8
Weerawut Chaiyasomboon <weerawut@live.com>
2024-03-07 10:07:15 +0700

    Initial Commit

---
+++ Shuzi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shuzi
+//
+//  Created by Weerawut Chaiyasomboon on 7/3/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shuzi/ShuziApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuziApp.swift
+//  Shuzi
+//
+//  Created by Weerawut Chaiyasomboon on 7/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ShuziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
